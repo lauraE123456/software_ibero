76c7e246a86a6c5df39101a0443dec8670570e4d
lauraE1234565 <laue0915@gmail.com>
2025-11-18 18:20:09 +0845

    new changes server

---
+++ server/index.js
@@ -0,0 +1,80 @@
+//get,post,put,delete
+const express = require("express");
+const cors = require("cors");
+const db = require("./db");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+PORT = 3001;
+//route for get all empleados
+app.get("/empleados", (req, res) => {
+  const sql = "SELECT * FROM empleados";
+  db.query(sql, (err, results) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ error: "error al obtener los datos del empleado" });
+    }
+    return res.json(results);
+  });
+});
+
+app.post("/empleados", (req, res) => {
+  const { nombre, edad, pais, cargo, anios } = req.body;
+  const sql =
+    "INSERT INTO empleados (nombre,edad,pais,cargo, anios) VALUES (?,?,?,?,?)";
+  db.query(sql, [nombre, edad, pais, cargo, anios], (err, result) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ error: "error al guardar los datos del empleado" });
+    }
+    return res.json({
+      message: "empleado guardado correctamente",
+      id: result.insertId,
+      nombre,
+      edad,
+      pais,
+      cargo,
+      anios,
+    });
+  });
+});
+
+app.put("/empleados/:id", (req, res) => {
+  const { id } = req.params;
+  const { nombre, edad, pais, cargo, anios } = req.body;
+  const sql =
+    "UPDATE empleados SET nombre=?,edad=?,pais=?,cargo=?,anios=? WHERE id=?";
+  db.query(sql, [id, nombre, edad, pais, cargo, anios], (err, result) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ error: "error al actualizar los datos del empleado" });
+    }
+    return res.json({
+      message: "empleado actualizado correctamente",
+    });
+  });
+});
+
+app.delete("/empleados/:id", (req, res) => {
+  const { id } = req.params;
+  const sql = "DELETE FROM empleados WHERE id=?";
+  db.query(sql, [id], (err, result) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ error: "error al eliminar los datos del empleado" });
+    }
+    return res.json({
+      message: "empleado eliminado correctamente",
+    });
+  });
+});
+
+app.listen(PORT, () => {
+  console.log("servidor del server corriendo en el puerto 3001");
+});
